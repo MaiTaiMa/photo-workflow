5d0197e7177fe2f29bc3b7d5936d38f5f5b31c4b
Matzethias <matthias.streser@coplanar.de>
2026-08-09 23:56:28 +0200

    clean up

--- photo-workflow/run_photo_workflow.sh
+++ photo-workflow/run_photo_workflow.sh
@@ -3,3 +3,4 @@ set -eu
 CONFIG_PATH="${1:-/app/config/config.yaml}"
 python /app/app/photo_workflow.py --config "$CONFIG_PATH" phase1
 python /app/app/photo_workflow.py --config "$CONFIG_PATH" phase2
+python /app/app/photo_workflow.py --config "$CONFIG_PATH" pipeline
